7b0c6022c46a0bd7c45b6832b8c2aea4148b3a2b
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2022-02-05 00:17:51 +0200

    Create test_denmune.py

---
+++ src/tests/test_denmune.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn_extra.cluster import KMedoids
+from sklearn.datasets import make_blobs
+from denmune import DenMune
+
+# test DenMune's results
+X_cc, y_cc = make_blobs(
+    n_samples=1000,
+    centers=np.array([[-1, -1], [1, 1]]),
+    random_state=0,
+    shuffle=False,
+    cluster_std=0.5,
+)
+
+
+def test_DenMune_results():
+    dm = DenMune(train_data=X_cc, train_truth=y_cc, k_nearest=10)
+    labels, validity = dm.fit_predict(show_analyzer=False)
+    # This test use data that are not perfectly separable so the
+    # accuracy is not 1. Accuracy around 0.90
+    assert (np.mean(dm.labels_pred == expected) > 0.90) or (1 - np.mean(dm.labels_pred == expected) > 0.90)
+
