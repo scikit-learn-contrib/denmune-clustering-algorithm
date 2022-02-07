206681f5d8bf12edb68e9a59cded1a7b98a6f7bc
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2022-02-07 04:40:17 +0200

    Update test_denmune.py

--- src/tests/test_denmune.py
+++ src/tests/test_denmune.py
@@ -34,14 +34,14 @@ def test_fit_predict_parameters(validate, show_plots, show_noise, show_analyzer)
 
     
 @pytest.mark.parametrize("train_data", [X_cc[:800] ])  
-@pytest.mark.parametrize("train_truth", [None, y_cc[:800] ])  
-@pytest.mark.parametrize("test_data", [None, X_cc[800:] ])  
-@pytest.mark.parametrize("test_truth", [None])  
+@pytest.mark.parametrize("train_truth", [y_cc[:800] ])  
+@pytest.mark.parametrize("test_data", [X_cc[800:] ])  
+@pytest.mark.parametrize("test_truth", [y_cc[800:] ])  
 def test_init_parameters(train_data, train_truth, test_data, test_truth):
     dm = DenMune(train_data=train_data, train_truth=train_truth, test_data=test_data, test_truth=test_truth, k_nearest=10)
     labels, validity = dm.fit_predict()
     # This test use data that are not perfectly separable so the
     # accuracy is not 1. Accuracy around 0.70
-    #assert (np.mean(dm.labels_pred == y_cc) > 0.70) or (1 - np.mean(dm.labels_pred == y_cc) > 0.70)    
+    #assert (np.mean(dm.labels_pred == y_cc) > 0.80) or (1 - np.mean(dm.labels_pred == y_cc) > 0.80)    
 
 
