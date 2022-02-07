e3275e17ab7fa94458741eeb2e260551e02f51f7
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2022-02-07 04:00:57 +0200

    Update test_denmune.py

--- src/tests/test_denmune.py
+++ src/tests/test_denmune.py
@@ -37,8 +37,8 @@ def test_fit_predict_parameters(validate, show_plots, show_noise, show_analyzer)
 @pytest.mark.parametrize("train_truth", [None, y_cc[:800] ])  
 @pytest.mark.parametrize("test_data", [None, X_cc[800:] ])  
 @pytest.mark.parametrize("test_truth", [None, y_cc[800:] ])  
-def test_init_parameters(train_data=train_data, train_truth=train_truth, test_data=test_data, test_truth=test_truth):
-    dm = DenMune(train_data=train_data, train_truth=y_cc, k_nearest=10)
+def test_init_parameters(train_data, train_truth, test_data, test_truth):
+    dm = DenMune(train_data=train_data, train_truth=train_truth, test_data=test_data, test_truth=test_truth, k_nearest=10)
     labels, validity = dm.fit_predict()
     # This test use data that are not perfectly separable so the
     # accuracy is not 1. Accuracy around 0.90
