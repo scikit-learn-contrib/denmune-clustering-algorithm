e1c8d5f1ae53990962670b9980b4061243c4618c
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2022-02-07 03:41:40 +0200

    Update test_denmune.py

--- src/tests/test_denmune.py
+++ src/tests/test_denmune.py
@@ -25,10 +25,24 @@ def test_DenMune_results():
 @pytest.mark.parametrize("show_plots", [True, False])  
 @pytest.mark.parametrize("show_noise", [True, False])  
 @pytest.mark.parametrize("show_analyzer", [True, False])  
-def test_DenMune_parameters(validate, show_plots, show_noise, show_analyzer):
+def test_fit_predict_parameters(validate, show_plots, show_noise, show_analyzer):
     dm = DenMune(train_data=X_cc, train_truth=y_cc, k_nearest=10)
     labels, validity = dm.fit_predict(validate=validate, show_plots=show_plots, show_noise=show_noise, show_analyzer=show_analyzer)
     # This test use data that are not perfectly separable so the
     # accuracy is not 1. Accuracy around 0.90
     assert (np.mean(dm.labels_pred == y_cc) > 0.90) or (1 - np.mean(dm.labels_pred == y_cc) > 0.90)    
+
+    
+@pytest.mark.parametrize("train_data", [None, X_cc[:800] ])  
+@pytest.mark.parametrize("train_truth", [None, y_cc[:800] ])  
+@pytest.mark.parametrize("test_data", [None, X_cc[800:] ])  
+@pytest.mark.parametrize("test_truth", [None, y_cc[800:] ])  
+
+def test_init_parameters(train_data=train_data, train_truth=train_truth, test_data=test_data, , test_truth=test_truth):
+    dm = DenMune(train_data=train_data, train_truth=y_cc, k_nearest=10)
+    labels, validity = dm.fit_predict()
+    # This test use data that are not perfectly separable so the
+    # accuracy is not 1. Accuracy around 0.90
+    assert (np.mean(dm.labels_pred == y_cc) > 0.90) or (1 - np.mean(dm.labels_pred == y_cc) > 0.90)    
+
 
