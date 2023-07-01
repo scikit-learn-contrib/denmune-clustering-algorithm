190c5734ab4fc9c00c8793d9a97298bb16d931bd
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-07-01 06:21:00 +0300

    Update test_denmune.py

--- src/tests/test_denmune.py
+++ src/tests/test_denmune.py
@@ -46,7 +46,7 @@ def test_parameters(train_data, train_truth, test_data, test_truth, validate, pr
                     labels, validity = dm.fit_predict(validate=validate, show_plots=show_plots, show_noise=show_noise, show_analyzer=show_analyzer)
                     # This test use data that are not perfectly separable so the
                     # accuracy is not 1. Accuracy around 0.70
-                    assert ( np.mean(dm.labels_pred == y_cc) > 0.80 or (1 - np.mean( dm.labels_pred == y_cc)  > 0.80) ) 
+                    assert ( np.mean(dm.labels_pred == y_cc) > 0.70 or (1 - np.mean( dm.labels_pred == y_cc)  > 0.70) ) 
 
 
 def test_DenMune_propagation():
