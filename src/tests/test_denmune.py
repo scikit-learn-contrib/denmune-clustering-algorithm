67907f21417a6989672aa1a69fa2ff6b209e53f8
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-07-01 08:30:04 +0300

    Update test_denmune.py

--- src/tests/test_denmune.py
+++ src/tests/test_denmune.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pytest
 from sklearn.datasets import make_blobs
 from sklearn.datasets import load_iris
-from denmune import DenMune
+from src.denmune import DenMune
 
        
 # test DenMune's results
@@ -34,7 +34,7 @@ def test_DenMune_results():
 @pytest.mark.parametrize("show_plots", [True, False])
 @pytest.mark.parametrize("show_noise", [True, False])
 @pytest.mark.parametrize("show_analyzer", [True, False])
-@pytest.mark.parametrize("prop_step", [0, 400])
+@pytest.mark.parametrize("prop_step", [0, 700])
 # all possible combinations will be tested over all parameters. Actually, 257 tests will be covered
 def test_parameters(train_data, train_truth, test_data, test_truth, validate, show_plots, show_noise, show_analyzer, prop_step):
     if not (train_data is None):
