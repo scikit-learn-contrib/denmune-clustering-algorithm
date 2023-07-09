3780c46879be4895d3097cfa92e63a8ab04a299d
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-07-09 03:02:00 +0300

    Update test_denmune.py

--- src/tests/test_denmune.py
+++ src/tests/test_denmune.py
@@ -37,24 +37,3 @@ def test_DenMune_results():
     assert (np.mean(dm.labels_pred == y_cc) > 0.80) or (1 - np.mean(dm.labels_pred == y_cc) > 0.80)
 
 
-@pytest.mark.parametrize("train_data", [None, train_data ])
-@pytest.mark.parametrize("train_truth", [None, train_labels ])  
-@pytest.mark.parametrize("test_data", [None, test_data ])  
-@pytest.mark.parametrize("test_truth", [None, test_labels ])
-@pytest.mark.parametrize("validate", [True, False])
-@pytest.mark.parametrize("show_plots", [True, False])
-@pytest.mark.parametrize("show_noise", [True, False])
-@pytest.mark.parametrize("show_analyzer", [True, False])
-@pytest.mark.parametrize("prop_step", [0, 0])
-
-# all possible combinations will be tested over all parameters. Actually, 257 tests will be covered
-def test_parameters(train_data, train_truth, test_data, test_truth, validate, show_plots, show_noise, show_analyzer, prop_step):
-    if not (train_data is None):
-        if not (train_data is not None and train_truth is None and test_truth is not None):
-            if not (train_data is not None and test_data is not None and train_truth is None):
-                 if not (train_data is not None and  train_truth is not None and test_truth is not None  and test_data is None):
-                    dm = DenMune(train_data=train_data, train_truth=train_truth, test_data=test_data, test_truth=test_truth, k_nearest=10,prop_step=prop_step)
-                    labels, validity = dm.fit_predict(validate=validate, show_plots=show_plots, show_noise=show_noise, show_analyzer=show_analyzer)
-                    # This test use data that are not perfectly separable so the
-                    # accuracy is not 1. Accuracy around 0.70
-                    assert ( np.mean(dm.labels_pred == y_cc) > 0.70 or (1 - np.mean( dm.labels_pred == y_cc)  > 0.70) ) 
